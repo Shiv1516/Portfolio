0f42da65fbb9d3e5a8cc4c72257086def4168faf
Shivnilay Srivastav <shivnilaysrivastav@gmail.com>
2024-12-30 01:44:47 +0530

    commit file

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import About from "./components/About";
+import Service from "./components/Service";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Service />
+      <Project />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
