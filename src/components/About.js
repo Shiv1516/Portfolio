c5c6a3961c4db208d544fe3c132c30219d32f9ea
Shivnilay Srivastav <shivnilaysrivastav@gmail.com>
2024-12-31 23:30:47 +0530

    file will be updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="wrapper df aic ptb40">
+      <div className="about-left p32">
+        <img src="/images/img_01.jpg" alt="img" className="br50 brd1" />
+      </div>
+      <div className="flx1 p24 lh30 fs20">
+        I'm a Frontend Developer with over a year of experience, specializing in
+        JavaScript, React, Next.js, HTML, CSS, and UI/UX development. I'm
+        currently work at AAJUX Design Studio, where I'm focus on building
+        user-friendly and visually appealing interfaces. With a strong
+        foundation in web development and design, I'm combine technical
+        expertise with creative solutions to deliver seamless user experiences.
+        My communication skills and problem-solving ability make I'm an asset in
+        collaborative environments.
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
