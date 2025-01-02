e39fc89d727be737da77f7b2a86ae0c6b5841291
Shivnilay Srivastav <shivnilaysrivastav@gmail.com>
2025-01-03 01:25:13 +0530

    file will be updated

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Service() {
-  return (
-    <div>Service</div>
-  )
+  return <div>Services</div>;
 }
 
-export default Service
+export default Service;
