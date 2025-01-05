3438dd0618fbdcdf9ce5f510a5afc8d160d1977b
Shivnilay Srivastav <shivnilaysrivastav@gmail.com>
2025-01-06 02:25:33 +0530

    file will be update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,70 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header wrapper v-center jcsb ptb24">
-      <Link to="/" className="logo df aic">
-        <img
-          src="/images/img_01.jpg"
-          alt="logo-img"
-          className="logo-icon brd1"
-        />
-        <h2 className="logo-test ttu fs20 fw6 pl16">INBIO</h2>
-      </Link>
-      <nav className="navbar v-center">
-        <ul id="nav-items" className="nav-items fc1 v-center fs14 fw5 fww">
-          <li className="nav-item">
-            <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
-              Home
+    <div
+      className={`header-section pr w100 zi99 bg1 transit1 ${
+        isScrolled ? "scrolled" : ""
+      }`}
+    >
+      <header className="header wrapper v-center jcsb ptb16">
+        <Link to="/" className="logo df aic">
+          <img
+            src="/images/img_01.jpg"
+            alt="logo-img"
+            className="logo-icon brd1"
+          />
+          <h2 className="logo-test ttu fs20 fw6 pl16">INBIO</h2>
+        </Link>
+        <nav className="navbar v-center">
+          <ul id="nav-items" className="nav-items fc1 v-center fs14 fw5 fww">
+            <li className="nav-item">
+              <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
+                Features
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
+                Portfolio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
+                Resume
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
+                Clients
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
+                Pricing
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
+                Blog
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <div>
+            <Link
+              to="/"
+              className="talk-btn fc3 transit2 br8 fs14 fw6 fc-h2 ttu plr16 ptb24 bg-bs-01 mlr12"
+            >
+              Buy Now
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
-              Features
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
-              Portfolio
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
-              Resume
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
-              Clients
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
-              Pricing
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
-              Blog
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-link fc-h1 transit2 ttu p16">
-              Contact
-            </Link>
-          </li>
-        </ul>
-        <div>
-          <Link
-            to="/"
-            className="talk-btn fc3 transit2 br8 fs14 fw6 fc-h2 ttu plr16 ptb24 bg-bs-01 mlr12"
-          >
-            Buy Now
-          </Link>
-        </div>
-      </nav>
-    </header>
+          </div>
+        </nav>
+      </header>
+    </div>
   );
 }
 
--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -3,13 +3,15 @@ import React from "react";
 function Pricing() {
   return (
     <div className="pricing-section wrapper ptb48 brd-b1 df fww">
-      <div className="pricing-card-container ps t0 flx40 tac">
-        <h4 className="section-subheading fs14 mb20 fc3 fw5 ls2 ttu">
-          What Clients Say
-        </h4>
-        <h2 className="section-heading fw7 fs60 mb32 fc2">Testimonial</h2>
+      <div className="pricing-card-container flx40 tac">
+        <div className="pricing-cards mr24 t-f ps">
+          <h4 className="section-subheading fs14 mb20 fc3 t-f ls2 ttu">
+            What Clients Say
+          </h4>
+          <h2 className="section-heading fw7 fs60 mb32 fc2">Testimonial</h2>
+        </div>
       </div>
-      {/* <div className="flx1">
+      <div className="flx1">
         <p>hi</p>
         <p>hi</p>
         <p>hi</p>
@@ -40,7 +42,73 @@ function Pricing() {
         <p>hi</p>
         <p>hi</p>
         <p>hi</p>
-      </div> */}
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+      </div>
     </div>
   );
 }
