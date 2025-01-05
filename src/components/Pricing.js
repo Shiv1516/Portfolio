bfd5eefa3611d9d756a0054047c687841e9376e5
Shivnilay Srivastav <shivnilaysrivastav@gmail.com>
2025-01-06 01:50:36 +0530

    file will be update

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from "react";
 
-function Pricing(){
-  return(
-    <div>Pricing</div>
-  )
+function Pricing() {
+  return (
+    <div className="pricing-section wrapper ptb48 brd-b1 df fww">
+      <div className="pricing-card-container ps t0 flx40 tac">
+        <h4 className="section-subheading fs14 mb20 fc3 fw5 ls2 ttu">
+          What Clients Say
+        </h4>
+        <h2 className="section-heading fw7 fs60 mb32 fc2">Testimonial</h2>
+      </div>
+      {/* <div className="flx1">
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+        <p>hi</p>
+      </div> */}
+    </div>
+  );
 }
-export default Pricing
+export default Pricing;
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { FaStar } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { ImArrowLeft2 } from "react-icons/im";
+import { ImArrowRight2 } from "react-icons/im";
 
 function Testimonial() {
   return (
@@ -8,7 +11,7 @@ function Testimonial() {
         What Clients Say
       </h4>
       <h2 className="section-heading fw7 fs60 mb32 fc2">Testimonial</h2>
-      <div className="testimonial-card-box ptb32 df fww">
+      <div className="testimonial-card-box ptb32 df aie fww w85 mlra pr">
         <div className="testimonial-person-detail flx33 zi3">
           <div className="person-details mr24 br12 p32 bg-bs-01 tal ">
             <div className="person-card-img mb40 ofh df br12">
@@ -27,10 +30,23 @@ function Testimonial() {
             <h3 className="person-card-dep fs14 mb16">Operating Officer</h3>
           </div>
         </div>
-        <div className="testimonial-des flx1 tal zi2">
-          <img src="/images/quote.png" alt="quote" className="quote-icon" />
-          <div className="testimonial-des-card ml12 bef pr bg-bs-01 br12 ptb48 plr48">
-            <div className="test-des-contend df jcsb fww aic brd-b1 pb40">
+        <div className="testimonial-des flx1 tal zi2 pr aft">
+          <div className="arrows df pa r0">
+            <Link
+              to="/"
+              className="left-icon p16 br8 fc3 fc-h2 transit2 bg-bs-01 mlr12"
+            >
+              <ImArrowLeft2 className="left-arrow fs20" />
+            </Link>
+            <Link
+              to="/"
+              className="right-icon p16 br8 fc3 fc-h2 transit2 bg-bs-01 mlr12"
+            >
+              <ImArrowRight2 className="right-arrow fs20" />
+            </Link>
+          </div>
+          <div className="testimonial-des-card ml12 bef pr bg-bs-01 br12 ptb40 plr40">
+            <div className="test-des-contend df jcsb fww aic brd-b1 pb32">
               <div className="test-des-card">
                 <h3 className="test-des-heading fs24 fw6 mb20">
                   Web App Development
@@ -47,7 +63,7 @@ function Testimonial() {
                 <FaStar className="mlr2 fc5" />
               </div>
             </div>
-            <p className="test-description mtb24 lh30 fw4 fs18">
+            <p className="test-description pt24 lh30 fw4 fs18">
               Important fact to nec sem ut imperdiet. Ut tincidunt est ac dolor
               aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in,
               lobortis mauris hendrerit ante. Ut tincidunt est ac dolor aliquam
